e36ac96a685c3b2e5caf674df9603b504783691b
Demetrius Johnson <contact@demetriusj.com>
2014-10-24 17:02:51 -0700

    feat: as a dev I want pellet internationalization to be accessible anywhere

--- components/internationalization/internationalization.jsx
+++ components/internationalization/internationalization.jsx
@@ -10,58 +10,85 @@ var spec = {
   missing: react.PropTypes.string
 };
 
+function getTranslation(locales, props) {
+
+  var state = {
+    hasErrors: false,
+    isMissing: true,
+    translation: ''
+  };
+
+  // todo: make a function that will walk over all the locales and try to match them i.e. us-en, us-br, us
+
+  if(locales) {
+    if(pellet.locales[locales]) {
+      if(props.key && pellet.locales[locales][props.key]) {
+        state.isMissing = false;
+        try {
+          state.translation = pellet.locales[locales][props.key](props);
+        } catch(ex) {
+          console.error('Cannot get translation because:', ex.message);
+          state.translation = '[ERROR:' + locales + ':' + props.key + ']';
+          state.hasErrors = true;
+        }
+      } else if(props.value) {
+        try {
+          var val = props.value.toLowerCase().replace(/\W/g, '');
+          if(pellet.locales[locales][val]) {
+            state.translation = pellet.locales[locales][val](props);
+          } else {
+            state.translation = props.value;
+          }
+
+          state.isMissing = false;
+        } catch(ex) {
+          console.error('Cannot get translation because:', ex.message);
+          state.translation = '[ERROR:' + locales + ':' + props.value + ']';
+          state.hasErrors = true;
+        }
+      } else {
+        state.translation = props.missing || 'MISSING';
+      }
+    } else {
+      state.translation = '[UNKNOWN LOCALE]';
+    }
+  } else {
+    state.translation = '[LOCALE NO SET]';
+  }
+
+  return state;
+}
+
+/**
+ * helper function for jade teammates making it easier to add intl to jade
+ *
+ * just add: "#{pellet.intlComponent({key:'MyKEY'})}"
+ *
+ * @param options
+ * @returns {Function}
+ */
+pellet.intlComponent = function(options) {
+  return function() {
+    return module.exports(options);
+  };
+}
+
+/**
+ * helper function to lookup translation in pellet
+ *
+ * @param scope
+ * @param options
+ * @returns {string|*|buildManifestMap.server.translation}
+ */
+pellet.intl = function(scope, options) {
+  return getTranslation(scope.props.locales || scope.context.locales || scope, options).translation;
+}
+
 module.exports = pellet.createClass({
   propsTypes: spec,
 
   getInitialState: function() {
-
-    var state = {
-      hasErrors: false,
-      isMissing: true,
-      translation: ''
-    };
-
-    var locales = this.props.locales || this.context.locales;
-
-    // todo: make a function that will walk over all the locales and try to match them i.e. us-en, us-br, us
-
-    if(locales) {
-      if(pellet.locales[locales]) {
-        if(this.props.key && pellet.locales[locales][this.props.key]) {
-          state.isMissing = false;
-          try {
-            state.translation = pellet.locales[locales][this.props.key](this.props);
-          } catch(ex) {
-            console.error('Cannot get translation because:', ex.message);
-            state.translation = '[ERROR:' + locales + ':' + this.props.key + ']';
-            state.hasErrors = true;
-          }
-        } else if(this.props.value) {
-          try {
-            var val = this.props.value.toLowerCase().replace(/\W/g, '');
-            if(pellet.locales[locales][val]) {
-              state.translation = pellet.locales[locales][val](this.props);
-            } else {
-              state.translation = this.props.value;
-            }
-
-            state.isMissing = false;
-          } catch(ex) {
-            console.error('Cannot get translation because:', ex.message);
-            state.translation = '[ERROR:' + locales + ':' + this.props.value + ']';
-            state.hasErrors = true;
-          }
-        } else {
-          state.translation = this.props.missing || 'MISSING';
-        }
-      } else {
-        state.translation = '[UNKNOWN LOCALE]';
-      }
-    } else {
-      state.translation = '[LOCALE NO SET]';
-    }
-
-    return state;
+    return getTranslation(this.props.locales || this.context.locales, this.props);
   },
 
   render: function() {
