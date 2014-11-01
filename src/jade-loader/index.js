04c16c534057a4769f316c030dd90e1e96573214
Demetrius Johnson <contact@demetriusj.com>
2014-10-31 21:12:53 -0700

    chore: copy the jade-react-compiler into pellet so we can update it AST logic

---
+++ src/jade-loader/index.js
@@ -0,0 +1,27 @@
+var parser = require('jade').Parser
+  , compiler = require('./compiler')
+
+// THANKS TO "Michael Phan-Ba" for this code https://github.com/mikepb/jade-react-compiler
+// We just needed to update to support the new react!
+
+module.exports = function(source) {
+  this.cacheable && this.cacheable();
+
+  var options = {
+    // todo: need to update this path using the source path info
+    filename:'/Users/demi/Projects/pellet/doghouse/frontend/haha'
+  };
+
+  var _parser = new parser(source.toString('utf8'), options.filename, options)
+  var _compiler = new compiler(_parser.parse(), options);
+
+  var jscode = _compiler.compile();
+  console.log(jscode)
+  jscode = 'React = react = require("react");' +
+    'Pellet = pellet = require("pellet");' +
+    'utils = {addComponent:pellet.jade_addComponent, addEl:pellet.jade_addEl, intl:pellet.jade_intl};' +
+    'module.exports=function(scope)' +
+    jscode.substring(25);
+
+  return jscode;
+};
