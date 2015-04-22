06583f249b06bb364e932f3d0c73d3e8f53dbc36
Demetrius Johnson <contact@demetriusj.com>
2015-04-22 16:12:36 -0700

    feat: as a dev I want to config that status code for 404 and 500 pages

--- bin/commands/cmd-server.js
+++ bin/commands/cmd-server.js
@@ -642,9 +642,9 @@ module.exports = function(program, addToReadyQue) {
 
             // if expressjs or nodejs
             if(res.status) {
-              res.status(404).send(appOptions.missingPage({config: appOptions, options:appOptions, req: req, res: res}));
+              res.status(appOptions.missingPageStatus || 404).send(appOptions.missingPage({config: appOptions, options:appOptions, req: req, res: res}));
             } else {
-              res.statusCode = 404;
+              res.statusCode = appOptions.missingPageStatus || 404;
               res.end(appOptions.missingPage({config: appOptions, req: req, res: res}));
             }
 
@@ -659,9 +659,9 @@ module.exports = function(program, addToReadyQue) {
 
             // if expressjs or nodejs
             if(res.status) {
-              res.status(500).send(appOptions.errorPage({config: appOptions, options:appOptions, req: req, res: res, err: err}));
+              res.status(appOptions.errorPageStatus || 500).send(appOptions.errorPage({config: appOptions, options:appOptions, req: req, res: res, err: err}));
             } else {
-              res.statusCode = 500;
+              res.statusCode = appOptions.errorPageStatus || 500;
               res.end(appOptions.errorPage({config: appOptions, req: req, res: res, err: err}));
             }
 
